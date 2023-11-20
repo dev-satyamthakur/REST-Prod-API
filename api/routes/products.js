5f44136c4fc6dabcf221912fad7ba35212c30846
Satyam Thakur <sat.thakur20@gmail.com>
2023-11-20 18:50:42 +0530

    Adding mongoose validation and returning for info

--- api/routes/products.js
+++ api/routes/products.js
@@ -7,15 +7,30 @@ const Product = require('../models/product.js')
 
 router.get('/', (req, res, next) => {
     Product.find()
-    .exec()
-    .then(docs => {
-        console.log(docs);
-        res.status(200).json(docs);
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({error: err});
-    })
+        .select('name price _id')
+        .exec()
+        .then(docs => {
+            const response = {
+                count: docs.length,
+                products: docs.map(doc => {
+                    return {
+                        name: doc.name,
+                        price: doc.price,
+                        _id: doc._id,
+                        request: {
+                            type: 'GET',
+                            url: 'http://localhost:3000/products/' + doc._id
+                        }
+                    }
+                })
+            };
+            console.log(docs);
+            res.status(200).json(response);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json
+        })
 });
 
 router.post('/', (req, res, next) => {
@@ -28,17 +43,43 @@ router.post('/', (req, res, next) => {
 
     productNew.save().then(result => {
         console.log(result);
-    }).catch(err => console.log(err));
+        res.status(201).json({
+            message: 'Product created successfully!',
+            createdProduct: {
+                name: result.name,
+                price: result.price,
+                _id: result._id,
+                request: {
+                    type: 'GET',
+                    url: 'http://localhost:3000/products/' + result._id
+                }
+            }
+        })
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({ error: err });
+    });
 
-    res.status(200).json({
-        message: 'Handling POST requests to /products',
-        createdProduct: productNew
-    })
+    
 });
 
 router.get('/:productId', (req, res, next) => {
     const id = req.params.productId;
     Product.findById(id)
+        .exec()
+        .then(doc => {
+            console.log(doc);
+            res.status(200).json(doc);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: err });
+        })
+});
+
+router.delete('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    Product.findByIdAndDelete(id)
         .exec()
         .then(doc => {
             console.log(doc);
