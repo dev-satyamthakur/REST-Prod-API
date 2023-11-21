ce0a9be550111a8328f7c294294a2de59851b411
Satyam Thakur <sat.thakur20@gmail.com>
2023-11-21 20:32:29 +0530

    Adding post a order endpoint and get all orders

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,22 +1,51 @@
 const express = require('express');
 const router = express.Router();
+const Order = require('../models/order.js');
+const mongoose = require('mongoose');
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({ // 200 means OK
-        message: 'Orders were fetched'
-    })
+    Order.find()
+        .select('product quantity _id')
+        .exec()
+        .then(docs => {
+            res.status(200).json({
+                count: docs.length,
+                orders: docs.map(doc => {
+                    return { 
+                        _id: doc._id,
+                        product: doc.product,
+                        quantity: doc.quantity,
+                        request: {
+                            type: 'GET',
+                            url: 'http://localhost:3000/orders/' + doc._id
+                        }
+                    }
+                })
+            });
+        })
+        .catch(err => {
+            res.status(500).json({ error: err });
+        });
 });
 
 router.post('/', (req, res, next) => {
-    const order = {
-        productId: req.body.productId,
-        quantity: req.body.quantity
-    }
+    const order = new Order({
+        _id: new mongoose.Types.ObjectId(),
+        quantity: req.body.quantity,
+        product: req.body.productId
+    });
 
-    res.status(200).json({ // 200 means OK
-        message: 'Orders created',
-        order: order
-    })
+    order.save()
+        .then(result => {
+            console.log(result);
+            res.status(200).json(result);
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({
+                error: err
+            })
+        })
 });
 
 module.exports = router;
