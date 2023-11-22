96fbfcc3ccb97c5fca26aab2a9aafeddfe1d1db0
Satyam Thakur <sat.thakur20@gmail.com>
2023-11-22 17:47:22 +0530

    Adding order deleted endpoints

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -31,6 +31,27 @@ router.get('/', (req, res, next) => {
         });
 });
 
+router.get("/:orderId", (req, res, next) => {
+    Order.findById(req.params.orderId).exec()
+        .then(order => {
+            if (!order) {
+                return res.status(404).json({
+                    message: "Order not found"
+                });
+            }
+            res.status(200).json({
+                order: order,
+                request: {
+                    type: 'GET',
+                    url: 'http://localhost:3000/orders'
+                }
+            });
+        })
+        .catch(err => {
+            res.status(500).json({ error: err });
+        });
+});
+
 router.post('/', async (req, res, next) => {
     try {
         const product = await Product.findById(req.body.productId);
@@ -64,6 +85,23 @@ router.post('/', async (req, res, next) => {
             error: err.message || "An error occurred while processing your request"
         });
     }
+});
+
+router.delete("/:orderId", (req, res, next) => {
+    Order.deleteOne({ _id: req.params.orderId }).exec()
+        .then(result => {
+            res.status(200).json({
+                message: 'Order deleted',
+                request: {
+                    type: 'POST',
+                    url: 'http://localhost:3000/orders',
+                    body: { productId: 'ID', quantity: 'Number' }
+                }
+            })
+        })
+        .catch(err => {
+            res.status(500).json({ error: err });
+        });
 });
 
 
