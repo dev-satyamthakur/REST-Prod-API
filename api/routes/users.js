00b566bbda1365c56ccebadeb3d16a2a6f622730
Satyam Thakur <sat.thakur20@gmail.com>
2023-11-27 14:50:08 +0530

    Adding checks in user signup

--- api/routes/users.js
+++ api/routes/users.js
@@ -7,26 +7,35 @@ const User = require('../models/user.js');
 
 router.post('/signup', (req, res, next) => {
 
-    bcrypt.hash(req.body.password, 10, (err, hash) => {
-        if (err) {
-            return res.status(500).json({ error: err });
-        } else {
-            const user = new User({
-                _id: new mongoose.Types.ObjectId(),
-                email: req.body.email,
-                password: hash
-            });
-            user.save()
-                .then(result => {
-                    console.log(result);
-                    res.status(201).json({ message: 'User created successfully!' });
-                })
-                .catch(err => {
-                    console.log(err);
-                    res.status(500).json({ error: err });
+    User.find({ email: req.body.email })
+        .exec()
+        .then(user => {
+            if (user.length >= 1) {
+                return res.status(409).json({ message: 'Email already exists!' });
+            }
+            else {
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    if (err) {
+                        return res.status(500).json({ error: err });
+                    } else {
+                        const user = new User({
+                            _id: new mongoose.Types.ObjectId(),
+                            email: req.body.email,
+                            password: hash
+                        });
+                        user.save()
+                            .then(result => {
+                                console.log(result);
+                                res.status(201).json({ message: 'User created successfully!' });
+                            })
+                            .catch(err => {
+                                console.log(err);
+                                res.status(500).json({ error: err });
+                            });
+                    }
                 });
-        }
-    });
+            }
+        });
 
 });
 
