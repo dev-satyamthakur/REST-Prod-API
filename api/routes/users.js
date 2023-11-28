64207df005e6d1f37ae507695377241517ccad3a
Satyam Thakur <sat.thakur20@gmail.com>
2023-11-28 17:21:41 +0530

    Adding jwt sign in that generates the token

--- api/routes/users.js
+++ api/routes/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken");
+require('dotenv').config();
 
 const User = require('../models/user.js');
 
@@ -53,7 +55,19 @@ router.post('/login', (req, res, next) => {
                 }
 
                 if (result) {
-                    return res.status(200).json({ message: "Auth successful" });
+                    const token = jwt.sign({
+                        email: user[0].email,
+                        userId: user[0]._id
+                    },
+                        process.env.JWT_KEY,
+                        {
+                            expiresIn: "1h"
+                        });
+
+                    return res.status(200).json({
+                        message: "Auth successful",
+                        token: token
+                    });
                 }
 
                 res.status(401).json({ message: 'Auth failed!' });
